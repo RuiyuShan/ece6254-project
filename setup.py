6280f01c6c7cfca0e6c38da76f7a2900252a0f53
Ruiyu Shan <shanruiyu@outlook.com>
2022-04-26 18:31:45 +0800

    update

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='ece6254-project',
+    version='',
+    packages=['GMM', 'GUI'],
+    url='',
+    license='',
+    author='shanruiyu',
+    author_email='',
+    description=''
+)
