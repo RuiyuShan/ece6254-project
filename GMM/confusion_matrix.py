4167126d1246b037c60f279bc896356ecf2691cb
Ruiyu Shan <shanruiyu@outlook.com>
2022-05-02 06:13:04 +0800

    update

---
+++ GMM/confusion_matrix.py
@@ -0,0 +1,10 @@
+import pickle
+
+from gmmtest import generate_confusion_table
+
+
+if __name__ == '__main__':
+    with open('/home/ruiyushan/proj/data_6273/confusion_table_05020551.pickle', 'rb') as handle:
+        dic = pickle.load(handle)
+
+    generate_confusion_table(dic, "05020551")
